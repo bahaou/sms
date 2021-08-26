00ddc5bc6b8c7ea9e89ca94e9b051245e5341b50
baha eddine weslati <54307698+bahaou@users.noreply.github.com>
2021-08-26 13:46:58 +0100

    Create nusoap3.php

---
+++ nusoap3.php
@@ -0,0 +1,24 @@
+<?php
+function send($identifier,$message,$appcode){
+require_once('lib1/nusoap.php');
+
+$wsdl = "http://10.120.6.174:9010/Common/SMS.svc?wsdl";
+$client = new nusoap_client($wsdl, 'wsdl');
+$err = $client->getError();
+if ($err) {
+   echo '<h2>L\'erreur!</h2>' . $err;
+   exit();
+}
+
+
+$result=$client->call('sendSMS', array('appCode'=>'CLD','messageBody'=>'baha','mobileNo'=>'+21695860772'));
+
+
+    print_r($result);
+
+
+}
+
+send('0530186545','test','CLD');
+
+?>
